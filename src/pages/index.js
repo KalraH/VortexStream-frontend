448d3a48fbf599eda60309a137038c43560a7591
Hariom Kalra <kalrahariom2001@gmail.com>
2025-08-04 11:38:00 +0530

    Added developed Channels into index.js of channels.

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Channel from "./channel/channel";
+import ChannelTweets from "./channel/channelTweets";
+import ChannelVideos from "./channel/channelVideos";
+import ChannelPlaylist from "./channel/channelPlaylist";
+import ChannelSubscribers from "./channel/channelSubscribers";
+
+export {
+        Channel,
+        ChannelTweets,
+        ChannelVideos,
+        ChannelPlaylist,
+        ChannelSubscribers,
+};
