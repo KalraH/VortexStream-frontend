446a007ef8f425969650c663a94f5852917d3a19
Hariom Kalra <kalrahariom2001@gmail.com>
2025-08-05 19:07:08 +0530

    Removed base_url log.

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,2 @@
 export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-// Add validation
-if (!BASE_URL) {
-        console.error("❌ VITE_BACKEND_URL is not defined!");
-        console.log("Available env vars:", import.meta.env);
-} else {
-        console.log("✅ BASE_URL loaded:", BASE_URL);
-}
